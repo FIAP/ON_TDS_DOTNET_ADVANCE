593453c05b2a36907cb515f54503476f145f13ed
Flavio Moreni <PF0954@fiap.com.br>
2024-05-04 17:30:25 -0300

    migration da entidade de representante

--- Fiap.Web.Alunos/Data/Contexts/DatabaseContext.cs
+++ Fiap.Web.Alunos/Data/Contexts/DatabaseContext.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Fiap.Web.Alunos.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fiap.Web.Alunos.Data.Contexts
 {
     public class DatabaseContext : DbContext
     {
+
+        // PROPRIEDADE PARA MANIPULAR A ENTIDADE DE REPRESENTANTE
+        public DbSet<RepresentanteModel> Representantes { get; set; }
+
+
+        // MÉTODO UTILIZADO PARA CRIAÇÃO DOS ELEMENTOS NO BANCO DE DADOS
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RepresentanteModel>(entity =>
+            {
+                // Definindo um nome para tabela
+                entity.ToTable("Representantes");
+
+                // Definindo chave primária
+                entity.HasKey(e => e.RepresentanteId);
+
+                // Tornando o nome obrigatório
+                entity.Property(e => e.NomeRepresentante).IsRequired();
+
+                // Adicionando índice único para CPF
+                entity.HasIndex(e => e.Cpf).IsUnique(); 
+            });
+        }
+
         public DatabaseContext(DbContextOptions options) : base(options)
         {
         }

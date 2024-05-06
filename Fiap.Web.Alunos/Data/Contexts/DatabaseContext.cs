8061674746cba0bb9f762ec58193c88025511b05
Flavio Moreni <PF0954@fiap.com.br>
2024-05-05 21:55:10 -0300

    adicionando cliente e seu relacionamento

--- Fiap.Web.Alunos/Data/Contexts/DatabaseContext.cs
+++ Fiap.Web.Alunos/Data/Contexts/DatabaseContext.cs
@@ -9,24 +9,43 @@ namespace Fiap.Web.Alunos.Data.Contexts
         // PROPRIEDADE PARA MANIPULAR A ENTIDADE DE REPRESENTANTE
         public DbSet<RepresentanteModel> Representantes { get; set; }
 
+        // PROPRIEDADE PARA MANIPULAR A ENTIDADE DE CLIENTE
+        public DbSet<ClienteModel> Clientes { get; set; }
 
-        // MÉTODO UTILIZADO PARA CRIAÇÃO DOS ELEMENTOS NO BANCO DE DADOS
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<RepresentanteModel>(entity =>
             {
-                // Definindo um nome para tabela
                 entity.ToTable("Representantes");
-
-                // Definindo chave primária
                 entity.HasKey(e => e.RepresentanteId);
-
-                // Tornando o nome obrigatório
                 entity.Property(e => e.NomeRepresentante).IsRequired();
-
-                // Adicionando índice único para CPF
                 entity.HasIndex(e => e.Cpf).IsUnique(); 
             });
+
+            modelBuilder.Entity<ClienteModel>(entity =>
+            {
+                // Define o nome da tabela para 'Clientes'
+                entity.ToTable("Clientes"); 
+                entity.HasKey(e => e.ClienteId); 
+                entity.Property(e => e.Nome).IsRequired(); 
+                entity.Property(e => e.Email).IsRequired();
+
+                // Especifica o tipo de dado para DataNascimento
+                entity.Property(e => e.DataNascimento).HasColumnType("date");
+                entity.Property(e => e.Observacao).HasMaxLength(500);
+
+                // Configuração da relação com RepresentanteModel
+                // Define a relação de um para um com RepresentanteModel
+                entity.HasOne(e => e.Representante)
+                    // Indica que um Representante pode ter muitos Clientes
+                    .WithMany()
+                    // Define a chave estrangeira
+                    .HasForeignKey(e => e.RepresentanteId)
+                    // Torna a chave estrangeira obrigatória
+                    .IsRequired(); 
+            });
+
         }
 
         public DatabaseContext(DbContextOptions options) : base(options)
